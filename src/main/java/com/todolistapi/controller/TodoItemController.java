230785b91033f319020d311fb47a9a7d8631a868
BARON MANZANO <MANZABA@OOCL.COM>
2020-11-03 01:11:08 +0800

    [CORS][Baron] feature: add get and post for controller

--- src/main/java/com/todolistapi/controller/TodoItemController.java
+++ src/main/java/com/todolistapi/controller/TodoItemController.java
@@ -1,9 +1,15 @@
 package com.todolistapi.controller;
 
+import com.todolistapi.dto.TodoItemRequest;
+import com.todolistapi.dto.TodoItemResponse;
 import com.todolistapi.mapper.TodoItemMapper;
+import com.todolistapi.model.TodoItem;
 import com.todolistapi.service.TodoItemService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/todos")
@@ -14,5 +20,18 @@ public class TodoItemController {
     public TodoItemController(TodoItemService todoItemService, TodoItemMapper todoItemMapper) {
         this.todoItemService = todoItemService;
         this.todoItemMapper = todoItemMapper;
+    }
+
+    @GetMapping
+    public List<TodoItemResponse> getAll() {
+        List<TodoItem> todoItems = todoItemService.getAll();
+        return todoItems.stream().map(todoItemMapper::toResponse).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TodoItemResponse addTodoItem(@RequestBody TodoItemRequest todoItemRequest) {
+        TodoItem todoItem =todoItemService.create(todoItemMapper.toEntity(todoItemRequest));
+        return todoItemMapper.toResponse(todoItem);
     }
 }
--- src/main/java/com/todolistapi/service/TodoItemService.java
+++ src/main/java/com/todolistapi/service/TodoItemService.java
@@ -1,15 +1,24 @@
 package com.todolistapi.service;
 
+import com.todolistapi.model.TodoItem;
 import com.todolistapi.repository.TodoItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TodoItemService {
-
     private TodoItemRepository todoItemRepository;
 
     public TodoItemService(TodoItemRepository todoItemRepository) {
         this.todoItemRepository = todoItemRepository;
     }
 
+    public List<TodoItem> getAll() {
+        return todoItemRepository.findAll();
+    }
+
+    public TodoItem create(TodoItem item) {
+        return todoItemRepository.save(item);
+    }
 }
